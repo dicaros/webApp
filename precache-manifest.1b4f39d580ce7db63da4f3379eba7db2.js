2063aa93b1b256748f61c37715c58987875731fd
dicaros <stedicaro-1@yahoo.it>
2020-01-06 20:02:52 +0100

    Updates

---
+++ precache-manifest.1b4f39d580ce7db63da4f3379eba7db2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b496836813372802ffea1fc52b3d8e",
+    "url": "/webApp/index.html"
+  },
+  {
+    "revision": "4bd1ed4544375d1bd3e2",
+    "url": "/webApp/static/css/main.6893540e.chunk.css"
+  },
+  {
+    "revision": "df998664fae5ffe6a508",
+    "url": "/webApp/static/js/2.0306289e.chunk.js"
+  },
+  {
+    "revision": "4bd1ed4544375d1bd3e2",
+    "url": "/webApp/static/js/main.cf9e04ee.chunk.js"
+  },
+  {
+    "revision": "0387cf894fde6020eb54",
+    "url": "/webApp/static/js/runtime-main.c708c745.js"
+  }
+]);
